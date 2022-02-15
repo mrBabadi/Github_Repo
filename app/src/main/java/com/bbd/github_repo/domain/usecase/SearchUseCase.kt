e4385fa0cb224fa78ffa1c79131ca67611aa0209
ali <ali.bap74@gmail.com>
2022-02-15 10:41:49 +0330

    implement usecases

--- app/src/main/java/com/bbd/github_repo/domain/usecase/SearchUseCase.kt
+++ app/src/main/java/com/bbd/github_repo/domain/usecase/SearchUseCase.kt
@@ -1,16 +1,28 @@
 package com.bbd.github_repo.domain.usecase
 
+import com.bbd.github_repo.data.LocalException
 import com.bbd.github_repo.data.ResponseState
+import com.bbd.github_repo.data.repository.ApiRepository
 import com.bbd.github_repo.domain.entity.UserSearchEntity
+import com.bbd.github_repo.domain.ext.toUserSearchEntity
 import io.reactivex.Flowable
+import io.reactivex.schedulers.Schedulers
+import javax.inject.Inject
 
 interface SearchUserUseCase {
     operator fun invoke(input:String) : Flowable<ResponseState<UserSearchEntity>>
 }
 
-class SearchUserUseCaseImpl : SearchUserUseCase {
+class SearchUserUseCaseImpl @Inject constructor(private val apiRepository: ApiRepository) : SearchUserUseCase {
+
     override fun invoke(input: String): Flowable<ResponseState<UserSearchEntity>> {
-        TODO("Not yet implemented")
+        return apiRepository.searchUser(input).toFlowable()
+            .map {response -> ResponseState.success(response.toUserSearchEntity()) }
+            .startWith(ResponseState.loading())
+            .onErrorReturn { exception ->
+                ResponseState.error(LocalException( throwable = exception))
+            }
+            .subscribeOn(Schedulers.io())
     }
 
 }
--- app/src/main/java/com/bbd/github_repo/domain/usecase/UserDetailsUseCase.kt
+++ app/src/main/java/com/bbd/github_repo/domain/usecase/UserDetailsUseCase.kt
@@ -1,16 +1,29 @@
 package com.bbd.github_repo.domain.usecase
 
+import com.bbd.github_repo.data.LocalException
 import com.bbd.github_repo.data.ResponseState
+import com.bbd.github_repo.data.repository.ApiRepository
 import com.bbd.github_repo.domain.entity.UserDetailEntity
+import com.bbd.github_repo.domain.ext.toUserDetailsEntity
 import io.reactivex.Flowable
+import io.reactivex.schedulers.Schedulers
+import javax.inject.Inject
 
 interface GetUserDetailsUseCase {
     operator fun invoke(userName: String): Flowable<ResponseState<UserDetailEntity>>
 }
 
-class GetUserDetailsUseCaseImpl : GetUserDetailsUseCase{
+class GetUserDetailsUseCaseImpl @Inject constructor(private val apiRepository: ApiRepository) :
+    GetUserDetailsUseCase {
+
     override fun invoke(userName: String): Flowable<ResponseState<UserDetailEntity>> {
-        TODO("Not yet implemented")
+        return apiRepository.getUserDetails(userName).toFlowable()
+            .map { data -> ResponseState.success(data.toUserDetailsEntity()) }
+            .startWith(ResponseState.loading())
+            .onErrorReturn { exception ->
+                ResponseState.error(LocalException(exception))
+            }
+            .subscribeOn(Schedulers.io())
     }
 
 }
