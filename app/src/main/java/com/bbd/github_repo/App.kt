f16b9130f33eca728d1ff76c364ac1625721b5df
ali <ali.bap74@gmail.com>
2022-02-15 10:44:52 +0330

    Add App class

---
+++ app/src/main/java/com/bbd/github_repo/App.kt
@@ -0,0 +1,19 @@
+package com.bbd.github_repo
+
+import android.app.Application
+import android.content.Context
+import androidx.multidex.MultiDex
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application() {
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
