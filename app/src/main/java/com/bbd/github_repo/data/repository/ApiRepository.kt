f984abeb4bc2be925a24cd2c81871d65007645c1
ali <ali.bap74@gmail.com>
2022-02-15 10:41:22 +0330

    Add Api Repository

--- app/src/main/java/com/bbd/github_repo/data/repository/ApiRepository.kt
+++ app/src/main/java/com/bbd/github_repo/data/repository/ApiRepository.kt
@@ -1,4 +1,11 @@
 package com.bbd.github_repo.data.repository
 
-class ApiRepository {
+import com.bbd.github_repo.data.source.remote.webservice.GithubWebServices
+import javax.inject.Inject
+
+class ApiRepository @Inject constructor(private val githubWebServices: GithubWebServices) {
+
+    fun searchUser(input: String) = githubWebServices.searchUsers(input)
+
+    fun getUserDetails(userName:String) = githubWebServices.getUserDetails(userName)
 }
